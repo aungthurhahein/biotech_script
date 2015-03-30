70f9df5cda5d9dd1c053fe439942d69495f8b01c
aungthurhahein <aungthurhahein@gmail.com>
2015-03-30 22:20:46 +0700

    home work synced

---
+++ numpy_tutorials.py
@@ -0,0 +1,8 @@
+__author__ = 'atrx'
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+df= pd.read_tsv("/home/atrx/c01_trinotate_annotation_report.xls")
+df.head(10)
--- py_scripts/c01_3cdhit_classify.py
+++ py_scripts/c01_3cdhit_classify.py
@@ -32,18 +32,26 @@ clstr_expand = []
 # expand representative sequences from second cdhit
 for cluster in clst_group_list:
     cluster_split = cluster.split('\t')
-    for member in cluster_split[1:]:
+    new_expand_clst = []
+    new_expand_clst.append(cluster_split[0])
+    for member in cluster_split:
+        # print member
         for two_cluster in two_cdhit_clst_list:
             two_cluser_split = two_cluster.split('\t')
+            # unique members
             cluster_list = []
             for i in two_cluser_split:
                 if i not in cluster_list:
                     cluster_list.append(i)
+
             for two_member in cluster_list[1:]:
                 if member.strip() == two_member.strip():
-                    clstr_expand.append(cluster_list)
-            else:
-                clstr_expand.append(member)
+                    for x_list in cluster_list[1:]:
+                        new_expand_clst.append(x_list)
+                else:
+                    if member not in new_expand_clst:
+                        new_expand_clst.append(member)
+    clstr_expand.append(new_expand_clst)
 
 for x in clstr_expand:
     print x
